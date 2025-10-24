5c752ff70035744646df068b2712ec9d777908d3
levifreire7 <levifreire7@gmail.com>
2025-10-23 23:09:56 -0300

    Implementando os endpoints de tarefas

--- todo_list_fastapi/app.py
+++ todo_list_fastapi/app.py
@@ -2,13 +2,14 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from todo_list_fastapi.routers import auth, users
+from todo_list_fastapi.routers import auth, todos, users
 from todo_list_fastapi.schemas import Message
 
 app = FastAPI(title='To Do List Project')
 
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(todos.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
